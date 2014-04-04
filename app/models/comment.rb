446eb50859dead684df679ded4ef0ca2960b4d05
PerseP <persep@gmail.com>
2014-04-04 23:20:43 +0200

    Add  Validations to app/models/comment.rb and app/models/post.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
 	# 1 post has many comments and 1 comment belongs to 1 post
 	belongs_to :post	
+
+	# validation
+	validates_presence_of :post_id
+	validates_presence_of :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
 	# 1 post has many comments and 1 comment belongs to 1 post
-	# we add  a  destroy  dependency so that the  comments 
-	# associated  with  a  post  will  get  destroyed  whenever  
-	# a  post  is  destroyed
+	# comments associated with a post will get destroyed whenever 
+	# a post is destroyed
 	has_many :comments, dependent: :destroy
+
+	# validation
+	validates_presence_of :title
+	validates_presence_of :body
+	
 end
