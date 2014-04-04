8a9e322cc0a02c3859e862831635c4f1438a0032
PerseP <persep@gmail.com>
2014-04-04 21:40:47 +0200

    Add Post has_many comments association and destroy dependency

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ActiveRecord::Base
+	# 1 post has many comments and 1 comment belongs to 1 post
+	belongs_to :post	
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+	# 1 post has many comments and 1 comment belongs to 1 post
+	# we add  a  destroy  dependency so that the  comments 
+	# associated  with  a  post  will  get  destroyed  whenever  
+	# a  post  is  destroyed
+	has_many :comments, :dependent => :destroy	
 end
